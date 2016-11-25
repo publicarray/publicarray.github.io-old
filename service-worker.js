7f493f708a36f94d18bfcb6d08fe9a0b17a8eaf7
Sebastian Schmidt <publicarray@icloud.com>
2016-11-25 18:23:16 +1000

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
 ---
 ---
-'use strict';
-const version = '{{site.time | date: "%Y%m%d%H%M%S"}}';
-const CACHE_NAME = 'cache-v'+version;
-var urlsToCache = [
+// https://serviceworke.rs/
+
+// Name of the caches used in this version of the service worker.
+const PRECACHE = 'precache-v{{site.time | date: "%Y%m%d%H%M%S"}}';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
   '/',
   '/service-worker.js',
   '/favicon.ico',
@@ -11,8 +14,6 @@ var urlsToCache = [
   '/blog/',
   '/contact/',
   '/projects/',
-  '/assets/workintegratedlearing.png',
-  '/assets/solarsystem.jpg',
   '/assets/light.css',
   '/assets/reveal.js',
   '/assets/script.js',
@@ -20,82 +21,79 @@ var urlsToCache = [
 ];
 
 var fallback = new Response('Sorry, you appear to be offline and the resource is not cached.', {
-  headers: {'Content-Type': 'text/plain'}
+  headers: {
+    'Content-Type': 'text/plain'
+  }
 });
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ https://googlechrome.github.io/samples/service-worker/basic/index.html
+*/
+
+// Names of the runtime cache, items will be added once the user has visited
+// new resources on the same domain.
+const RUNTIME = 'runtime';
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  // empty runtime and old versions of the cache
+  const currentCaches = [PRECACHE];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest)
-          .then(function(response) {
-            // Check if we received a valid response
-            if (!response || !response.ok || response.status !== 200 || response.type !== 'basic') {
-              console.error('Response status ' + response.status, response.url);
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
               return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-
+            });
           }).catch(function(e) {
-            console.error('Fetch failed. returning offline page instead.', e);
+            console.error('Failed to fetch; ' + e.toString());
             return fallback;
-            // return caches.open(OFFLINE_CACHE).then(function(cache) {
-            //   return cache.match(OFFLINE_URL);
-            // });
           });
-
+        });
       })
-  );
+    );
+  }
 });
 
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
