7e8097f068aaa79b0821fd047eae9eedb76d5e3c
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-02-03 07:35:53 +1000

    Add a CSP

--- _assets/js/inline.js
+++ _assets/js/inline.js
@@ -12,6 +12,23 @@ function domReady(fn) {
   }
 }
 
+// https://github.com/report-uri/report-uri-js
+// Thanks Scott Helme!
+document.addEventListener('securitypolicyviolation', function (event) {
+    var config = {'reportUri': 'https://publicarray.report-uri.com/r/d/csp/enforce'}
+    var reportKeys = {'blockedURI':'blocked-uri', 'columnNumber':'column-number', 'documentURI':'document-uri', 'effectiveDirective':'effective-directive', 'lineNumber':'line-number', 'originalPolicy':'original-policy', 'sourceFile':'source-file', 'statusCode':'status-code', 'violatedDirective':'violated-directive'};
+    var json = {'csp-report': {}};
+    for (var i = 0, len = config.keys.length; i < len; i++) {
+        if (event[config.keys[i]] !== 0 && event[config.keys[i]] !== '') {
+            json['csp-report'][(reportKeys[config.keys[i]] ? reportKeys[config.keys[i]] : config.keys[i])] = event[config.keys[i]];
+        }
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', config.reportUri, true);
+    xhr.setRequestHeader('content-type', 'application/csp-report');
+    xhr.send(JSON.stringify(json));
+});
+
 (function () { // don't pollute DOM with global vars
   'strict mode'
   // check for js, transition and transform support
